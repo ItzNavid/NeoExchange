c3e75ab7b46ab7305410c96dbffab8cf7aaf6824
BuildTools <unconfigured@null.spigotmc.org>
2025-11-11 20:56:31 +0100

    Added Neo Stone and Neo Plate with names and textures

---
+++ src/main/java/com/badiei/neoexchange/items/NeoItems.java
@@ -0,0 +1,18 @@
+package com.badiei.neoexchange.items;
+
+import com.badiei.neoexchange.NeoExchange;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class NeoItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(NeoExchange.MOD_ID);
+
+    public static final DeferredItem<Item> NEO_STONE = ITEMS.registerItem("neo_stone", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> NEO_PLATE = ITEMS.registerItem("neo_plate", Item::new, new Item.Properties());
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
